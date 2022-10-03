af3c104a46be2df25c4a786a5a81e79f0d332954
Marc Seitz <marc@mfts.io>
2022-10-03 11:21:29 -0400

    initial commit
    - bootstrap create-next-app with tailwind and typescript
    - add prettier and eslint

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html className="min-h-screen bg-white" lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
