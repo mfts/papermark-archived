a68ff58e4f54d5d8dd636f275b2bdc67018cd093
Marc Seitz <marc@mfts.io>
2022-10-03 11:23:50 -0400

    add prisma and next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL;
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  cookies: {
+    sessionToken: {
+      name: `${VERCEL_DEPLOYMENT ? "__Secure-" : ""}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
+        domain: VERCEL_DEPLOYMENT ? ".papermark.io" : undefined,
+        secure: VERCEL_DEPLOYMENT,
+      },
+    },
+  },
+  callbacks: {
+    session: async ({ session, token }) => {
+      session.user = {
+        // @ts-ignore
+        id: token.sub,
+        ...session.user,
+      };
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
